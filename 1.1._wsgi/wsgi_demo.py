9f95ce7c7edeb4bc62da148247a2a4dbf2939fc6
hjp709394@gmail.com <hjp709394@gmail.com>
2018-05-10 13:23:44 -0700

    Sample WSGI

--- 1.1._wsgi/wsgi_demo.py
+++ 1.1._wsgi/wsgi_demo.py
@@ -1,7 +1,16 @@
 from wsgiref.simple_server import make_server
 import argparse
+from pprint import pprint
 
 def application(environ, start_response):
+    ### uncommend the following code if you want to check what is inside environ and start_response
+    # print('environ: \n')
+    # pprint(environ)                   # environ is a dict that contains most of the information 
+                                        # you need to decide how to response a request
+    # print('\nstart_response: \n')
+    # print(start_response)
+    # print('\n\n')
+
     status = '200 OK'                   # Status code ref: https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
     headers = [('Content-Type', 'text/html; charset=utf8')]
     start_response(status, headers)
@@ -10,8 +19,8 @@ def application(environ, start_response):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Simple SWGI demo')
-    parser.add_argument('--ip', type=str, required=True, help='Your ip address')
-    parser.add_argument('--port', type=int, required=True, help='Your ip address')
+    parser.add_argument('--ip', type=str, required=True, help='ip address')
+    parser.add_argument('--port', type=int, required=True, help='port')
     args = parser.parse_args()
     return args
 
